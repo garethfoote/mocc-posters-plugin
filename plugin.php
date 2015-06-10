147edb17cca7a3e3f0237ca64940ff820974f718
garethfoote <gareth.foote@gmail.com>
2015-06-10 15:34:58 +0100

    Updating name of plugin

--- plugin.php
+++ plugin.php
@@ -4,7 +4,7 @@
  * @wordpress-plugin
  * Plugin Name:       MoCC Posters
  * Plugin URI:        http://moccguide.net/
- * Description:       A plugin for creating poster pages and displaying statistics about it's vists.
+ * Description:       A plugin for creating MoCC Poster webpages. It transparently records and displays statistics about tracking and page analytics.
  * Version:           1.0.0
  * Author:            Gareth Foote
  * Author URI:        http://garethfoote.co.uk
